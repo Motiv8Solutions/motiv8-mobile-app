b9d1e3975affe6c90322fa7dbd34b1964b3130a1
Sunil Abhyankar <abhyankar_sunil@hotmail.com>
2019-06-12 22:52:42 -0700

    Added biometric screen with ability to check availability

--- src/Screens/SignupScreen.jsx
+++ src/Screens/SignupScreen.jsx
@@ -290,6 +290,172 @@ const PasswordEntryStyled = styled(injectIntl(PasswordEntry))`
     }
 `;
 
+class BiometricScreen extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            biometricsSupported: false
+        };
+        this.handleBiometricUnavailable = this.handleBiometricUnavailable.bind(this);
+        this.checkBiometricSupport = this.checkBiometricSupport.bind(this);
+        this.checkIOSBiometricSupport = this.checkIOSBiometricSupport.bind(this);
+        this.checkAndroidBiometricSupport = this.checkAndroidBiometricSupport.bind(this);
+    }
+
+    /**
+     * Check for device support for biometric authentication.
+     * First launch the correct plugin based on the type of device, iOS or Andriod. We do not support anything else, right now.
+     */
+    componentDidMount () {
+        this.checkBiometricSupport({
+            ios: this.checkIOSBiometricSupport,
+            android: this.checkAndroidBiometricSupport
+        })
+    }
+
+    render () {
+        return (
+            <div className={this.props.className}>
+                <Label type='heading1' content={this.props.intl.formatMessage({id: 'COMPANY_TITLE'})} alternate={true}/>
+                {this.renderAuthentication()}
+            </div>
+        )
+    }
+
+    renderAuthentication () {
+        if (this.state.biometricsSupported) {
+            return (
+                <>
+                    <Label type='body1' content={this.props.intl.formatMessage({id: 'BIOMETRIC_MESSAGE_OPT_IN'})} alternate={true}/>
+                    <div className='button'>
+                        <PrimaryButton text={this.props.intl.formatMessage({id: 'SET_UP_BIOMETRIC_AUTH'}, {type: 'Touch Id'})}
+                            clickHandler={this.registerBiometrics}/>
+                    </div>
+                    <Label type='body1' content={this.props.intl.formatMessage({id: 'BIOMETRIC_MESSAGE_OPT_OUT'})} alternate={true}/>
+                    <div className='button'>
+                        <PrimaryButton text={this.props.intl.formatMessage({id: 'OPT_OUT'})} clickHandler={this.handleBiometricsOptOut}/>
+                    </div>
+                </>
+            );
+        }
+        return (
+            <>
+                <Label type='body1' content={this.props.intl.formatMessage({id: 'BIOMETRIC_MESSAGE_UNAVAILABLE'})} alternate={true}/>
+                <div className='button'>
+                    <PrimaryButton text={this.props.intl.formatMessage({id: 'OK'})} clickHandler={this.handleBiometricUnavailable}/>
+                </div>
+            </>
+        );
+    }
+
+    /**
+     * Function called when the OK button acknowledging the unavailability of the biometric screen is clicked.
+     * The user has 'logged in' using the code, take them to the HOME screen.
+     */
+    handleBiometricUnavailable () {
+        console.info('Biometrics unavailablility acknowledged, taking the user home.');
+        this.props.history.push('/home');
+    }
+
+    /**
+     * Function called when the user opts out of biometric authentication and the Opt out button is clicked.
+     * The user has 'logged in' using the code, take them home.
+     */
+    handleBiometricsOptOut () {
+        console.info('Biometrics opt out acknowledged, taking the user home.');
+        this.props.history.push('/home');
+    }
+
+    /**
+     * Function called when the user wants to register for biometric authentication.
+     * Call the Cordova plugin that launches the biometric registration UI.
+     */
+    registerBiometrics () {
+
+    }
+
+    /**
+     * Helper function that identifies the device type (iOS vs Andriod) and calls the appropriate function that checks for bimetric availability.
+     * @param {func} biometricFunctions A function map of device type and biometric availability functions.
+     */
+    checkBiometricSupport (biometricFunctions) {
+        if (navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+            if (typeof biometricFunctions['ios'] === 'function') {
+                biometricFunctions['ios']();
+            }
+        } else if (navigator.userAgent.toLowerCase().indexOf('andriod') > 0) {
+            if (typeof biometricFunctions['android'] === 'function') {
+                biometricFunctions['andrioid']();
+            }
+        } else {
+            console.info(`Platform not recognized while checking for biometric auth availability.`);
+            this.setState({
+                biometricsSupported: false
+            });
+        }
+    }
+
+    checkIOSBiometricSupport () {
+        let that = this;
+        window.plugins.touchid.isAvailable(
+            function (type) {
+                // biometric auth is available
+                console.info(`iOS biometric auth is available, type is ${type}`);
+                that.setState({
+                    biometricsSupported: true
+                });
+            },
+            function (message) {
+                // error function, biometric auth is not available
+                console.warn(`Biometric auth is not available on iOS device. Message is ${JSON.stringify(message)}`);
+                that.setState({
+                    biometricsSupported: false
+                });
+            }
+        )
+    }
+
+    checkAndroidBiometricSupport () {
+        let that = this;
+        FingerprintAuth.isAvailable(
+            function (result) {
+                // biometric auth is available
+                console.info(`Android biometric auth is available, result is ${JSON.stringify(result)}`);
+                that.setState({
+                    biometricsSupported: true
+                });
+            },
+            function (message) {
+                // if (err === 'Cancelled') {
+                //     console.info(`Android fingerprint auth dialog cancelled`);
+                //     // TODO: Show warning message that this is the only form of authentication.
+                //     // with option to re-enroll
+                // }
+                // biometric auth is not available
+                console.warn(`Biometric auth is not available on Android device. Message is ${JSON.stringify(message)}`);
+                that.setState({
+                    biometricsSupported: false
+                });
+            }
+        )
+    }
+}
+
+const BiometricScreenStyled = styled(injectIntl(withRouter(BiometricScreen)))`
+    display: flex;
+    flex-direction: column;
+    flex: auto;
+    justify-content: center;
+
+    div.button {
+        display: flex;
+        justify-content: center;
+        margin-top: 16px;
+        margin-bottom: 32px;
+    }
+
+`;
+
 class SignupScreen extends React.Component {
     carousalRef = React.createRef();
 
@@ -327,8 +493,7 @@ class SignupScreen extends React.Component {
                             organizations={this.state.organizations}/>
                     </Slide>
                     <Slide>
-                        <PasswordEntryStyled bearerToken={this.state.bearerToken} carousal={this.carousalRef} onPasswordConfirm={this.handleSubmitPasswordResponse}
-                            countryCode={this.state.countryCode} mobileNumber={this.state.mobileNumber} organizations={this.state.organizations}/>
+                        <BiometricScreenStyled/>
                     </Slide>
                 </Carousel>
             </div>
